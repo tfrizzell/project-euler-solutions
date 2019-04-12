435ae04ae181a0589bf6bf2ee7b887aff8154828
tfrizzell <trevor.frizzell@mlt.ca>
2019-04-12 12:47:55 -0300

    Updated method of handling large numbers

--- problem-016.js
+++ problem-016.js
@@ -1,29 +1,24 @@
 console.info('Calculating solution for Project Euler Problem 16...\r\n');
 const start = Date.now();
 
-const sumDigits = (base, exponent) => {
-    const digits = [];
+const power = (base, exponent) => {
+    let value = '1',
+        carry = 0;
 
-    while (exponent > 0) {
-        exponent--;
+    while (exponent-- > 0) {
+        for (let i = 0; i < value.length; i++) {
+            const val = (value[i] * base) + carry;
+            carry = Math.floor(val / 10);
+            value = `${value.substr(0, i)}${val % 10}${value.substr(i + 1)}`;
 
-        if (digits.length === 0) {
-            digits[0] = 1;
-        }
-
-        for (let i = 0; i < digits.length; i++) {
-            digits[i] *= base;
-            (digits[i] >= 10) && (digits[i + 1] === undefined) && (digits[i + 1] = 0);
-
-            if (digits[i - 1] >= 10) {
-                digits[i] += Math.floor(digits[i - 1] / 10);
-                digits[i - 1] %= 10;
+            if (carry && value[i + 1] === undefined) {
+                value += 0;
             }
         }
     }
 
-    return digits.reduce((accumulator, value) => accumulator + value, 0);
+    return value.split('').reverse().join('');
 };
 
-console.log('Result:', sumDigits(2, 1000));
+console.log('Result:', power(2, 1000).split('').reduce((accumulator, value) => accumulator + parseInt(value), 0));
 console.log('\r\nExecution Time:', `${Date.now() - start}ms`);
